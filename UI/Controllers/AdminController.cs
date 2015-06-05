718277358ec8590bcebad5e2880721aa032d51cf
AlsySaf <Kantripul@mail.ru>
2015-06-05 17:42:37 +0300

    я пыталась сделать представление товаров по примеру, но здесь надо все менять + чуть-чуть поменялся Layout

---
+++ UI/Controllers/AdminController.cs
@@ -0,0 +1,61 @@
+﻿//чтобы мы могли принимать загруженное изображение и сохранять его в базе данных
+
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Web;
+//using System.Web.Mvc;
+
+//namespace UI.Controllers
+//{
+//    public class AdminController : Controller
+//    {
+//        //
+//        // GET: /Admin/
+//        public ActionResult Index()
+//        {
+//            return View();
+//        }
+//    }
+//}
+
+
+
+
+
+/*
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UI.Controllers
+{
+    public class AdminController : Controller
+    {
+        //
+        // GET: /Admin/
+        [HttpPost]
+        public ActionResult Edit(Products product, HttpPostedFileBase image)
+        {
+            if (ModelState.IsValid)
+            {
+                if (image != null)
+                {
+                    product.ImageMimeType = image.ContentType;
+                    product.ImageData = new byte[image.ContentLength];
+                    image.InputStream.Read(product.ImageData, 0, image.ContentLength);
+                }
+                repository.SaveProduct(product);
+                TempData["message"] = string.Format("{0} has been saved", product.Name);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                // there is something wrong with the data values
+                return View(product);
+            }
+        }
+    }
+}*/
