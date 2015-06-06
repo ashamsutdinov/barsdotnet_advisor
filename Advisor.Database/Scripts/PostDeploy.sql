2c6b32228f50559bc24d61414cd8f6829c579ccf
AlsySaf <Kantripul@mail.ru>
2015-06-06 18:16:27 +0300

    заполняю CreateProduct.sql

--- Advisor.Database/Scripts/PostDeploy.sql
+++ Advisor.Database/Scripts/PostDeploy.sql
@@ -1,6 +1,9 @@
 ﻿:r .\CreateCategories.sql
 go
 
+:r .\CreateProducts.sql
+go
+
 insert into [Users] ([Login],[PasswordHash],[Name],[Sirname],[Email],[Info])
 values (N'Вася',N'конечно',N'Вася',N'лучший',N'из',N'людей')
 go
