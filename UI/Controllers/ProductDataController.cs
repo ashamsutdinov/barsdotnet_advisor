554bafede546702e2918d120bc41a9521cfdf68d
Иванова Елена <sky.winter.cress@gmail.com>
2015-06-06 11:50:17 +0300

    Заработало добавление товара+валидация авторизации

--- UI/Controllers/ProductDataController.cs
+++ UI/Controllers/ProductDataController.cs
@@ -15,30 +15,48 @@ namespace UI.Controllers
         private readonly IProductManager _productManager
             = Services.Factory.Get<IProductManager>();
 
+        //главная страница продукта
         public ActionResult Index()
         {
             return View();
         }
-        public ActionResult Change()
+
+        //изменение продукта
+        public ActionResult Edit()
         {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Edit(ProductModel model)
+        {
+            //пусть пока сюда
+            return Redirect("/");
+        }
+
+
+        //на этот метод приходят user, когда вбивает в командной строке
+        public ActionResult Add()
+        {
+            if (CurrentUser == null)
+            {//надо зарегаться чтобы добавлять что-либо
+                //поэтому просто выкинем человека на главную Х)
+                return Redirect("/");
+            }
             return View();
         }
 
         [HttpPost]
         public ActionResult Add(ProductModel model)
         {
-            if (CurrentUser == null)
-            {//надо зарегаться чтобы добавлять что-либо
-                ModelState.AddModelError("Name", "Вы не зарегистрированы!!");
-                return View();
-            }
-            else
+            if (ModelState.IsValid)
             {
                 _productManager.Add(CurrentUser.Id, model.Name, model.Info, model.MinValue, model.MaxValue, model.Category, "");
-               //фотка нужна
+                //фотка нужна
                 //return Content("Ваш товар добавлен");
                 return Redirect("/");
             }
+            return View();
         }
     }
 }
