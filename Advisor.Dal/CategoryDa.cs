fc55abef3ace9644aff7527e732f954633d04a2d
Иванова Елена <sky.winter.cress@gmail.com>
2015-06-09 09:35:45 +0300

    Выпадающий список

--- Advisor.Dal/CategoryDa.cs
+++ Advisor.Dal/CategoryDa.cs
@@ -1,9 +1,17 @@
 ﻿using Advisor.Dal.Domain;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Advisor.Dal
 {
     public class CategoryDa:
         Dao<int,Category>
     {
+        public IEnumerable<Category> GetAllCategories()
+        {
+            return Select(p => true)
+                .OrderBy(p => p.Name)
+                .ToList();
+        }
     }
 }
